13e36e0c6cea9d23bbd708956fe671b05f1cb9c6
Laurcons <laurcons@outlook.com>
2025-11-07 00:42:13 +0200

    remove dist

--- dist/poll-scheduler.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnModuleInit } from '@nestjs/common';
-import { DiscordBotService } from './discord-bot.service';
-export declare class PollSchedulerService implements OnModuleInit {
-    private readonly discordBotService;
-    private readonly logger;
-    constructor(discordBotService: DiscordBotService);
-    onModuleInit(): Promise<void>;
-    handleDailyPoll(): Promise<void>;
-}
